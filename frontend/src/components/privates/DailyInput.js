8357ebfe6239b0dcf6b991038c9035edeb7d062a
Patricia Heimfarth <pbeier24@googlemail.com>
2021-12-26 21:44:29 +0100

    range input

--- frontend/src/components/privates/DailyInput.js
+++ frontend/src/components/privates/DailyInput.js
@@ -2,17 +2,25 @@ import { Component } from "react";
 import Nav from 'react-bootstrap/Nav';
 
 class DailyInput extends Component {
+
+    onInput() {
+        var input = document.getElementById("typeinp");
+        var currentVal = input.value;
+        this.setState({
+            value: currentVal
+        })
+        document.getElementById("amount").value = currentVal;
+    }
+
+
     render() {
 
         return (
             <div>
                 <div class="d-flex justify-content-center my-4">
-                    <label htmlFor="fruit" className="form-label">Fruit</label>
-                    <span class="font-weight-bold blue-text mr-2 mt-1">0</span>
-                    <form class="range-field w-50">
-                        <input min="0" max="3" step="0.5" id="fruit" className=" form-range border-0" type="range" />
-                    </form>
-                    <span class="font-weight-bold blue-text ml-2 mt-1">100</span>
+                    <label htmlFor="amountInput" className="form-label">Fruit</label>
+                    <input id="typeinp" type="range" min="0" max="3" step="0.5" defaultValue="3" onInput={this.onInput.bind(this)} />
+                    <output id="amount" name="amount" for="amountInput">0</output>
                 </div>
             </div>
 
