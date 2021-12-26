23a7d0113d587cf112738a576a8ff7981d9aa7a6
Patricia Heimfarth <pbeier24@googlemail.com>
2021-12-26 16:39:16 +0100

    register and login frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navigationbar from "./components/Navigationbar";
+import Home from "./components/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Register from "./components/Register";
+import Login from "./components/Login";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>My Dailies</h1>
         <Navigationbar></Navigationbar>
+        <Home></Home>
+        <Login></Login>
+        <Register></Register>
       </div>
     );
   }
