6e4b1cf2f14eb944f6293721eb1aa7539c393e99
Patricia Heimfarth <pbeier24@googlemail.com>
2021-12-26 13:59:37 +0100

    register and login

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
- 
+
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 app.use(bodyParser.json());
- 
+
 const db = require("./config/keys").mongoURI;
- 
+
 mongoose
   .connect(
     db,
@@ -19,5 +19,5 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
-const port = process.env.PORT || 5000;  
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
