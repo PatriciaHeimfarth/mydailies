99ff3001997f3f6d98b6c0e28ede4ca02348eeb8
Patricia Heimfarth <pbeier24@googlemail.com>
2021-12-26 15:01:36 +0100

    passport

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 const app = express();
+
+const user = require("./routes/user");
+
 
 app.use(
   bodyParser.urlencoded({
@@ -19,5 +23,10 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/user", user);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
