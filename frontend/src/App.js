c6610f1a4b88d8c206acc69921d440273e788ba2
Patricia Heimfarth <pbeier24@googlemail.com>
2021-12-26 16:47:33 +0100

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 import Navigationbar from "./components/Navigationbar";
 import Home from "./components/Home";
@@ -8,13 +10,14 @@ import Login from "./components/Login";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My Dailies</h1>
-        <Navigationbar></Navigationbar>
-        <Home></Home>
-        <Login></Login>
-        <Register></Register>
-      </div>
+      <Router>
+        <div className="App">
+          <Navigationbar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
