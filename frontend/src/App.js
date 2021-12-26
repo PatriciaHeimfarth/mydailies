d85bda5f7f394e584ec26b218a53c9cca20152c4
Patricia Heimfarth <pbeier24@googlemail.com>
2021-12-26 16:02:12 +0100

    adding bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navigationbar from "./components/Navigationbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>My Dailies</h1>
+        <Navigationbar></Navigationbar>
       </div>
     );
   }
