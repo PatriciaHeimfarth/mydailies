284aa6c25ef45ffd1f3a29a378ea30da069eaacb
Patricia Heimfarth <pbeier24@googlemail.com>
2021-12-26 14:57:16 +0100

    login route

--- routes/user.js
+++ routes/user.js
@@ -45,3 +45,56 @@ router.post("/register", (req, res) => {
         }
     });
 });
+
+// Login
+
+// @route POST user/login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email }).then(user => {
+
+        if (!user) {
+            return res.status(404).json({ emailnotfound: "Email not found" });
+        }
+
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    {
+                        expiresIn: 60 * 60 * 24 * 356
+                    },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+            } else {
+                return res
+                    .status(400)
+                    .json({ passwordincorrect: "Password incorrect" });
+            }
+        });
+    });
+});
+
+module.exports = router;
