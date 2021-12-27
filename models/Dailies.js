b30ebbe91cb023567096f5d7246a5ff4c669b6a3
Patricia Heimfarth <pbeier24@googlemail.com>
2021-12-27 15:19:17 +0100

    store dailies

--- models/Dailies.js
+++ models/Dailies.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const DailySchema = new Schema({
+  type: String,
+  date: Date,
+  amount: Number,
+  userId: String
+});
+
 const DailiesGroupSchema = new Schema({
   name: String,
-  dailies: mongoose.Schema.Types.Mixed
+  dailies: [DailySchema]
 });
+
+
+
+
 module.exports = Dailies = mongoose.model("dailies", DailiesGroupSchema);
